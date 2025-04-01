b2cc5d88348fe903e4fd80e46a363440efc413a7
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-03-31 20:43:02 -0600

    Agregue ejemplo del docente

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import Recorrido from './components/Recorrido.vue'
-// import RecorridoMio from './components/RecorridoMio.vue'
-// import Horario from './components/Horario.vue';
-// import Horario1 from './components/Horario1.vue';
-import Padre from './components/Props/Padre.vue';
-import padre1 from './components/Props1/padre1.vue';
+import Eventos from './components/Eventos/Eventos.vue'
 </script>
 
 <template>
-    <HelloWorld nombre="Edgar Mauricio Sarmiento" />
-    <HelloWorld nombre="Props Ejemplo" />
-  <!-- <Recorrido />
-  <RecorridoMio /> -->
-  <!-- <Horario /> -->
-  <!-- <Horario1 /> -->
-  <!-- <Padre /> -->
-  <padre1 />
-
-
+  <Eventos />
 </template>
 
 <style scoped>
---
+++ src/components/Eventos/Eventos.vue
@@ -0,0 +1,59 @@
+<template >
+   <div class="container">
+    <div class="row mt-4">
+        <div>
+            <h4>Evento Click</h4>
+            <button @click="click" class="btn btn-danger" type="button">Dale clic</button>
+            <button @click="soyButton" class="btn btn-warning mx-2" type="button">Dale clic 2</button>
+        </div>
+        <div>
+            <h4>Evento change</h4>
+            <label class="input-group-text" for="">Ingresa tu nombre</label>
+            <input class="form-control mt-2" type="text" id="nombre" @change="nombreE">
+            <p>El nombre ingresado es: <strong>{{ nombre }}</strong></p>
+        </div>
+
+        <div>
+            <h3>Evento de mouseenter y mouseleave</h3>
+        <div  class="text-center"
+         style="color: white; width: 300px; height: 200px; background-color: black;text-align: center;padding: 15px;border-radius: 14px;"
+          @mouseenter="mostrarMensaje" @mouseleave="ocultarMensaje">
+         <p>Pase el mouse</p>   
+         <p>{{ mensaje }}</p>
+
+        </div>
+        </div>
+    </div>
+   </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            nombre:'',
+            mensaje:'',
+        }
+    },
+   methods:{ 
+    nombreE(event){
+        this.nombre = event.target.value
+    },
+    click() {
+        alert("Gracias por darle clic")
+    },
+    soyButton(){
+        alert("Soy un boton")
+    },
+   
+    mostrarMensaje(){
+        this.mensaje="El mouse esta dentro del rectangulo"
+    },
+    ocultarMensaje(){
+        this.mensaje="El mouse esta fuera del rectangulo"
+    },
+   }
+}
+</script>
+<style >
+    
+</style>
